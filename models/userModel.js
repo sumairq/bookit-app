0a3e84956de25405ec4ca163c2935626297e60f2
sumairq <sumairqaisar992@gmail.com>
2025-11-18 21:18:48 +0500

    Implement route protection controller

--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,7 @@ const userSchema = mongoose.Schema({
       message: 'Passwords are not the same.',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -51,6 +52,21 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimeStamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    console.log(changedTimeStamp, JWTTimestamp);
+    return JWTTimestamp < changedTimeStamp; // 300 < 200
+  }
+
+  // False means NOt changed vid-132
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
