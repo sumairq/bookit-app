4fb330ceb7005a9226e79d650d1d8bcc2307accd
sumairq <sumairqaisar992@gmail.com>
2025-11-23 08:46:46 +0500

    Use rate limiter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError.js');
 const globalErrorHandler = require('./controllers/errorController.js');
@@ -7,10 +8,18 @@ const tourRouter = require('./routes/tourRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
 
 const app = express();
-// 1) MIDDLEWARES
+// 1) GlOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
 // Added this line because it's default is 'simple' in express@5 unlike the previous versions.
 app.set('query parser', 'extended');
 //This middleware makes the req.query mutable like it was in express versions older than 5.
