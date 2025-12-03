c3508520279e7891f4c2bb6d90f3c0c413a34ef3
sumairq <sumairqaisar992@gmail.com>
2025-12-03 05:09:20 +0500

    Finish file upload

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,25 +1,65 @@
-// const fs = require('fs');
-const Tour = require('../models/tourModel');
-const AppError = require('../utils/appError');
-// const AppError = require('./../utils/appError');
+const multer = require('multer');
+const sharp = require('sharp');
+const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
+const AppError = require('./../utils/appError');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
+const multerStorage = multer.memoryStorage();
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
   }
-  next();
 };
 
-exports.aliasTopFiveTours = (req, res, next) => {
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+// upload.single('image') req.file
+// upload.array('images', 5) req.files
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  next();
+});
+
+exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
@@ -27,33 +67,15 @@ exports.aliasTopFiveTours = (req, res, next) => {
 };
 
 exports.getAllTours = factory.getAll(Tour);
-
 exports.getTour = factory.getOne(Tour, { path: 'reviews' });
-
 exports.createTour = factory.createOne(Tour);
-
 exports.updateTour = factory.updateOne(Tour);
-
 exports.deleteTour = factory.deleteOne(Tour);
-
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   }
-//   res.status(204).json({
-//     status: 'success',
-//     data: null,
-//   });
-// });
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
     {
-      $match: {
-        ratingsAverage: { $gte: 4.5 },
-      },
+      $match: { ratingsAverage: { $gte: 4.5 } },
     },
     {
       $group: {
@@ -70,7 +92,7 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
       $sort: { avgPrice: 1 },
     },
     // {
-    //   $match: {_id: {$ne: 'EASY'}}
+    //   $match: { _id: { $ne: 'EASY' } }
     // }
   ]);
 
@@ -115,8 +137,11 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     {
       $sort: { numTourStarts: -1 },
     },
-    { $limit: 12 },
+    {
+      $limit: 12,
+    },
   ]);
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -125,9 +150,8 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
   });
 });
 
-// '/tours-within/:distance/center/:latlng/unit/:unit',
-// /tours/tours-within/233/center/34.111745,-118.113491/unit/mi
-
+// /tours-within/:distance/center/:latlng/unit/:unit
+// /tours-within/233/center/34.111745,-118.113491/unit/mi
 exports.getToursWithin = catchAsync(async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
@@ -137,7 +161,7 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
   if (!lat || !lng) {
     next(
       new AppError(
-        'Please provide latitude and longitude in the format lat,lang.',
+        'Please provide latitutr and longitude in the format lat,lng.',
         400
       )
     );
@@ -165,7 +189,7 @@ exports.getDistances = catchAsync(async (req, res, next) => {
   if (!lat || !lng) {
     next(
       new AppError(
-        'Please provide latitude and longitude in the format lat,lang.',
+        'Please provide latitutr and longitude in the format lat,lng.',
         400
       )
     );
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const tourController = require('../controllers/tourController');
-const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
-const reviewRouter = require('../routes/reviewRoutes');
+const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
+
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-//POST /tour/234234ad4/reviews
-//POST /tour/234234ad4/reviews
-//POST /tour/234234ad4/reviews/923423dfda
+// POST /tour/234fad4/reviews
+// GET /tour/234fad4/reviews
 
-// We are basically saying whenever you see a route like this use reviewRouter
 router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
-  .get(tourController.aliasTopFiveTours, tourController.getAllTours);
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
 router
@@ -30,13 +28,10 @@ router
 router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getToursWithin);
+// /tours-within?distance=233&center=-40,45&unit=mi
+// /tours-within/233/center/-40,45/unit/mi
 
 router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
-
-// Create a checkBody middleware
-//Check if body contains the name and price property
-//If not, send back 400 (bad request)
-// Add it to the post handler stack
 
 router
   .route('/')
@@ -44,7 +39,6 @@ router
   .post(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
-    tourController.checkBody,
     tourController.createTour
   );
 
@@ -54,6 +48,8 @@ router
   .patch(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.updateTour
   )
   .delete(
